00f2ac18125c4d556e015854e94043093014fc43
Alex Newman <posix4e@gmail.com>
2016-11-21 08:24:11 -0800

    Repair linux build
        Basic building works.
        https://github.com/brave/electron/issues/94 for outstanding
        issues.

--- brave/common/brave_paths.cc
+++ brave/common/brave_paths.cc
@@ -5,35 +5,22 @@
 #include "brave/common/brave_paths.h"
 
 #include "base/lazy_instance.h"
+#include "base/files/file_path.h"
 #include "base/path_service.h"
 #include "base/files/file_path.h"
 #include "browser/brightray_paths.h"
 #include "build/build_config.h"
 #include "chrome/common/chrome_paths_internal.h"
+#include <memory>
 
 namespace brave {
   bool GetDefaultAppDataDirectory(base::FilePath* result) {
-#if defined(OS_LINUX)
-    std::unique_ptr<base::Environment> env(base::Environment::Create());
-    if (!env)
-      return false
-
-    result = base::nix::GetXDGDirectory(env.get(),
-                                      base::nix::kXdgConfigHomeEnvVar,
-                                      base::nix::kDotConfigDir);
-    if (result.BaseName.value() != base::nix::kDotConfigDir)
-      return false;
-
-    return true;
-#else
-  return PathService::Get(brightray::DIR_APP_DATA, result);
-#endif
+    return PathService::Get(brightray::DIR_APP_DATA, result);
   }
 
   bool GetDefaultUserDataDirectory(base::FilePath* result) {
 #if defined(OS_MACOSX)
     // on mac we SetOverrideVersionedDirectory to fix GetVersionedDirectory
-    return chrome::GetDefaultUserDataDirectory(result);
 #else
     if (!GetDefaultAppDataDirectory(result)) {
       return false;
@@ -41,5 +28,6 @@ namespace brave {
     result->Append(FILE_PATH_LITERAL("brave"));
     return true;
 #endif
+    return chrome::GetDefaultUserDataDirectory(result);
   }
 }
